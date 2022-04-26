847a55ad6ec91059989e76c53dd4c899f6b01905
Martijn Boland <martijn@taiga.nl>
2022-04-26 12:31:06 +0200

    Seed roles

--- extensions/seeder/src/index.js
+++ extensions/seeder/src/index.js
@@ -1,25 +1,32 @@
 const fs = require('fs').promises;
+const path = require('path');
 
-export default ({ filter, action }, { services, database }) => {
+export default ({ filter, action }, { services, database, getSchema }) => {
 
 	const { RolesService } = services;
 
-	async function readJsonFile(path) {
-		console.log(`Reading json file from ${path}`);
-		var jsonText = String(await fs.readFile(path));
+	async function readJsonFile(filePath) {
+		console.log(`Reading json file from ${filePath}`);
+		var jsonText = await fs.readFile(filePath);
 		return JSON.parse(jsonText);
 	}
 
 	async function seedRoles() {
-    console.log('Seeding roles...');
-		const roles = await readJsonFile('../../../seeddata/roles.json');
-		var rolesService = new RolesService();
-		var existingRoles = await rolesService.readByQuery();
+    
+		console.log('Seeding roles...');
+		
+		const roles = await readJsonFile(path.join(__dirname, '../../../seeddata/roles.json'));
+		const schema = await getSchema();
+
+		var rolesService = new RolesService({ knex: database, schema: schema });
+		var existingRoles = await rolesService.readByQuery({ fields: '*' });
+		
 		if (existingRoles.length <= 1) { // Only administrator
 			console.log('Only one role exists, creating other roles...');
 			await rolesService.upsertMany(roles);
+			console.log('Finished creating roles');
 		} else {
-			console.log('More than one role already exists, skipping seed...');
+			console.log('More than one role already exists, skipping seed => ', existingRoles);
 		}
 	}
 
